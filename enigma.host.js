ef6cbc1824b5cac13f84e53138640262b45d8134
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2020-03-03 17:18:18 +1300

    Update enigma.host.js

--- enigma.host.js
+++ enigma.host.js
@@ -1,4 +1,4 @@
-importURL('enigma.js');
-importURL('enigma-keys.txt');
+importURL('enigma.min.js', {runJSCode:true}).catch(function(e){throw e;});
+importURL('enigma-keys.txt').then((e)=>{window.keys = e.result;});
 function add(key){}
 function get(key, keyID, destKeyID){}
