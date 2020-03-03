d29d2cb329841beab095397f52a73ac37c85dc94
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2020-03-04 09:35:56 +1300

    Update import.js

--- import.js
+++ import.js
@@ -1,43 +1,5 @@
 function url2Object(URL){
-  var urlObject = {href:URL};
-  if(URL.includes('?')){
-    urlObject.search = '?'+(a=URL.split('?'), a.shift(), a.join('?'));
-  }else urlObject.search='';
-  if(URL.includes('#')){
-    urlObject.hash = '#'+(a=URL.split('#'), a.shift(), a.join('#'));
-  }else urlObject.hash='';
-  if(!urlObject.search&&!urlObject.hash){
-    urlObject.search = location.search;
-    urlObject.hash = location.hash;
-  }
-  if(URL.includes('//')){
-    b=URL.split('//');
-    urlObject.protocol = b[0];
-    urlObject.host = b[1].split('/')[0];
-    urlObject.pathname = (c=b[1].split('/'), c.shift(), c=c.join('/'), c.split('?')[0].split('#')[0]);
-  }else{
-    urlObject.host = URL.split('/')[0];
-    urlObject.pathname = (c=location.pathname + URL, c.split('?')[0].split('#')[0]);
-    if(!urlObject.host){
-      urlObject.host = location.host;
-      urlObject.protocol = location.protocol;
-    }else{
-      urlObject.protocol = 'https:';
-    }
-  }
-  if(urlObject.host){
-    urlObject.hostname = urlObject.host;
-    urlObject.origin = urlObject.protocol + '//' + urlObject.host;
-  }else{
-    urlObject.hostname = urlObject.origin = '';
-  }
-  if(urlObject.pathname[0]!='/'){urlObject.pathname='/'+urlObject.pathname;}
-  urlObject.port = '';
-  urlObject.href = urlObject.protocol+'//'+urlObject.host+urlObject.pathname+(urlObject.search?urlObject.search:urlObject.hash);
-  if(urlObject.pathname.split('/')[urlObject.pathname.split('/').length-1].includes('.')){
-    urlObject.fileext = '.'+urlObject.pathname.split('/')[urlObject.pathname.split('/').length-1].split('.').pop();
-  }else urlObject.fileext = undefined;
-  return urlObject;
+  if(!document.getElementById('locationFrame'))document.body.append((i=document.createElement('iframe'), i.src=URL, i.id='locationFrame', i.style.display='none', i));
 }
 function importURL(URL, options){
   options=options?options:{};
