dcbe74a453decb7c70b393ca071462ab34e053a8
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2020-03-06 12:56:57 +1300

    Update import.js

--- import.js
+++ import.js
@@ -22,11 +22,12 @@ function url2Object(URL){
                             s(i.contentWindow.location);
                           break;
                           default:
-                            f(x);
+                            f({xhr:x, attempt:'https://'+URL});
                           break;
                         }
                       }
                     };
+                    x.send();
                   };
                   i.src='https://'+URL;
                 break;
@@ -47,23 +48,26 @@ function url2Object(URL){
 function importURL(URL, options){
   options=options?options:{};
   var prom = new Promise((function(URL, options){return function(resolve, reject){
-    var result = {URL:url2Object(URL), options:options};
-    xhr=new XMLHttpRequest(), xhr.onreadystatechange=(function(xhr, resolve, reject, result, options){
-      if(xhr.readyState==4&&xhr.status==200){
-        switch(this.URL.fileext){
-          case '.js':
-            if(options.runJSCode)this.response = (new Function(xhr.response)).bind(window)();else this.response = xhr.response;
-          break;
-          default:
-            this.response = xhr.response;
-          break;
+    var result = {options:options};
+    url2Object(URL).then((r)=>{
+      result.URL = r;
+      xhr=new XMLHttpRequest(), xhr.onreadystatechange=(function(xhr, resolve, reject, result, options){
+        if(xhr.readyState==4&&xhr.status==200){
+          switch(this.URL.fileext){
+            case '.js':
+              if(options.runJSCode)this.response = (new Function(xhr.response)).bind(window)();else this.response = xhr.response;
+            break;
+            default:
+              this.response = xhr.response;
+            break;
+          }
+          resolve(result);
+        }else if(xhr.readyState==4){
+          reject(result);
         }
-        resolve(result);
-      }else if(xhr.readyState==4){
-        reject(result);
-      }
-    }).bind(result, xhr, resolve, reject, result, options), xhr.open("GET", result.URL.href), xhr.send();
-    result.xhr=xhr;
+      }).bind(result, xhr, resolve, reject, result, options), xhr.open("GET", result.URL.href), xhr.send();
+      result.xhr=xhr;
+    }).catch((e)=>{f(e)});
   };})(URL, options));
   return prom;
 }
